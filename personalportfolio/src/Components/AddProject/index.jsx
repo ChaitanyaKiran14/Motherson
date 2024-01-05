7fe7825c600b58d20827c8beb9129bf366a56dce
chaitanya <g.chaitanya1419@gmail.com>
2024-01-05 11:59:09 +0530

    Added functionality to  AddProject component and Projects component

--- personalportfolio/src/Components/AddProject/index.jsx
+++ personalportfolio/src/Components/AddProject/index.jsx
@@ -1,10 +1,118 @@
+import React, { useState } from 'react';
+import Projects from '../Projects';
+import { v4 as uuid } from 'uuid';
+
+
+const initialProjectsData = [
+  {
+    id: uuid(),
+    projectName: 'Project Name',
+    projectLink: 'https://example.com/project1',
+    imageUrl: 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    description: 'I created this personal project to show how to create an interface in Figma using a Portfolio as an example.',
+  },
+  
+];
+
 const AddProject = () => {
+  const [formData, setFormData] = useState({
+    projectName: '',
+    projectLink: '',
+    imageUrl: '',
+    description: '',
+  });
 
-    return(
-        <div>
-            <h1 className="text-center mt-8" > Add Project Form</h1>
+  const [projectsData, setProjectsData] = useState(initialProjectsData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newProject = {
+      id: uuid(),
+      ...formData,
+    };
+    setProjectsData((prevProjects) => [...prevProjects, newProject]);
+    
+    setFormData({
+      projectName: '',
+      projectLink: '',
+      imageUrl: '',
+      description: '',
+    });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold font-serif text-black-500 mb-6 border-b-4 border-yellow-500 pb-2">
+        Add Project
+      </h1>
+
+      <form
+        className="w-full max-w-md p-4 md:p-8 rounded-md relative"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Project Name</label>
+          <input
+            className="w-full h-12 md:h-14 border-2 rounded-md p-2 mt-2"
+            type="text"
+            placeholder="Enter project name"
+            name="projectName"
+            value={formData.projectName}
+            onChange={handleChange}
+          />
         </div>
-    )
 
-}
-export default AddProject
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Project Link</label>
+          <input
+            className="w-full h-12 md:h-14 border-2 rounded-md p-2 mt-2"
+            type="text"
+            placeholder="Enter project link"
+            name="projectLink"
+            value={formData.projectLink}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Image Url</label>
+          <input
+            className="w-full h-12 md:h-14 border-2 rounded-md p-2 mt-2"
+            type="text"
+            placeholder="Enter image url"
+            name="imageUrl"
+            value={formData.imageUrl}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Description</label>
+          <textarea 
+            className="w-full mb-5 h-32 md:h-40 border-2 rounded-md p-2 mt-2 overflow-auto"
+            placeholder="Enter project description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button
+          className="bg-yellow-500 text-white px-4 py-2 rounded-md absolute bottom-4 right-4"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+      
+      <Projects projectsData={projectsData} />
+    </div>
+  );
+};
+
+export default AddProject;
--- personalportfolio/src/Components/Projects/index.jsx
+++ personalportfolio/src/Components/Projects/index.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 
-export default function Projects() {
+export default function Projects({ projectsData }) {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold font-serif text-black-500 mb-6 border-b-4 border-yellow-500 pb-2">
         Projects
       </h1>
-      <div className="flex max-w-4xl flex-col items-center rounded-md border md:flex-row">
-        <div className="h-full w-full md:h-[350px] md:w-[400px]">
-          <img
-            src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            alt="Laptop"
-            className="h-full w-full rounded-md object-cover"
-          />
-        </div>
-        <div className="w-full">
-        <div className="p-4">
-          <h1 className="inline-flex items-center text-xl h-12 font-serif font-semibold" style={{ lineHeight: '3rem' }}>
-            Project Name
-          </h1>
-          <p className="mt-3 text-base text-gray-600">
-            I created this personal project to show how to create an interface in Figma using a Portfolio as an example.
-          </p>
-          <div className="mt-3 flex items-center space-x-2">
-            <button style={{ border: '1px solid black' }} className='mb-2 rounded-full px-4 py-2 text-base font-semibold text-gray-900 btn bg-gray-100' >View Project</button>
+      {projectsData ? (
+        projectsData.map((project) => (
+          <div key={project.id} className="flex flex-col items-center rounded-md border max-w-4xl md:flex-row mb-4">
+            <div className="w-full">
+              <div className="p-4">
+                <h1 className="inline-flex items-center text-xl h-12 font-serif font-semibold" style={{ lineHeight: '3rem' }}>
+                  {project.projectName}
+                </h1>
+                <p className="mt-3 text-base text-gray-600">
+                  {project.description}
+                </p>
+                <div className="mt-3 flex items-center space-x-2">
+                  <a href={project.projectLink} target="_blank" rel="noopener noreferrer" style={{ border: '1px solid black' }} className='mb-2 rounded-full px-4 py-2 text-base font-semibold text-gray-900 btn bg-gray-100' >View Project</a>
+                </div>
+              </div>
+            </div>
+            <div className="h-full w-full md:h-[350px] md:w-[400px]">
+              <img
+                src={project.imageUrl}
+                alt={project.projectName}
+                className="h-full w-full rounded-md object-cover"
+              />
+            </div>
           </div>
-        </div>
-      </div>
-
-      </div>
+        ))
+      ) : (
+        <p>No projects available</p>
+      )}
     </div>
   );
 }
